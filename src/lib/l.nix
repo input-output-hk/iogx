eb823a047414683f21b4e7a4eaed7d67105232b5
zeme <lorenzo.calegari@iohk.io>
2023-07-01 12:19:48 +0200

    WIP

--- src/lib/l.nix
+++ src/lib/l.nix
@@ -296,25 +296,25 @@ let
         final-str = l.concatStringsSep "\n" lines;
       in
       final-str;
-  };
 
 
-  # Stolen from https://github.com/divnix/nosys
-  deSystemize = let
-    iteration = cutoff: system: fragment:
-      if ! (l.isAttrs fragment) || cutoff == 0 then 
-        fragment
-      else 
-        let recursed = l.mapAttrs (_: iteration (cutoff - 1) system) fragment; in 
-        if l.hasAttr "${system}" fragment then
-          if l.isFunction fragment.${system} then 
-            recursed // { __functor = _: fragment.${system} ;}
-          else 
-            recursed // fragment.${system}
+    # Stolen from https://github.com/divnix/nosys
+    deSystemize = let
+      iteration = cutoff: system: fragment:
+        if ! (l.isAttrs fragment) || cutoff == 0 then 
+          fragment
         else 
-          recursed;
-  in
-    iteration 3;
+          let recursed = l.mapAttrs (_: iteration (cutoff - 1) system) fragment; in 
+          if l.hasAttr "${system}" fragment then
+            if l.isFunction fragment.${system} then 
+              recursed // { __functor = _: fragment.${system} ;}
+            else 
+              recursed // fragment.${system}
+          else 
+            recursed;
+    in
+      iteration 3;
+  };
 
 in
 

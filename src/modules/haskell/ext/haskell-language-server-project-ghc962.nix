b21437ac42bea1afcc5f73826d289d010d21f4d8
zeme <lorenzo.calegari@iohk.io>
2023-08-23 17:23:55 +0200

    wip

--- src/modules/haskell/ext/haskell-language-server-project-ghc962.nix
+++ src/modules/haskell/ext/haskell-language-server-project-ghc962.nix
@@ -2,22 +2,12 @@
 
 pkgs.haskell-nix.cabalProject' {
 
-  # See https://github.com/haskell/haskell-language-server/issues/411.
-  # We want to use stylish-haskell, hlint, and implicit-hie as standalone tools
-  # *and* through HLS. But we need to have consistent versions in both cases,
-  # otherwise e.g. you could format the code in HLS and then have the CI
-  # complain that it's wrong
-  #
-  # The solution we use here is to:
-  # a) Where we care (mostly just formatters), constrain the versions of
-  #    tools which HLS uses explicitly
-  # b) Pull out the tools themselves from the HLS project so we can use
-  #    them elsewhere
   cabalProjectLocal = ''
     package shake-bench
       buildable: False
-    benchmarks: Falses
+    benchmarks: False
   '';
+
   # constraints: stylish-haskell==0.14.2.0, hlint==3.4.1
 
   src = l.fetchTarball {
@@ -25,15 +15,14 @@ pkgs.haskell-nix.cabalProject' {
     sha256 = "sha256:1ivqj503al44nnilmpqd916ds5cl7hcxy4jm94ahi8y13v9p8r7y";
   };
 
-  # Cannot build with 9.6.2 because shake-bench.cabal has this constraint:
-  # if impl(ghc >= 9.5)
-  #   buildable: False
   compiler-nix-name = "ghc962";
 
   modules = [{
-    # packages.shake-bench.buildable = false;
-    # packages.shake-bench.buildable = false;
+    # packages.shake-bench.buildable = l.mkForce false;
+    # packages.haskell-language-server.buildable = l.mkForce false;
+    # packages.haskell-language-server.components.benchmarks.benchmark.buildable = l.mkForce false;
     # See https://github.com/haskell/haskell-language-server/pull/1382#issuecomment-780472005
+
     packages.ghcide.flags.ghc-patched-unboxed-bytecode = true;
   }];
 }

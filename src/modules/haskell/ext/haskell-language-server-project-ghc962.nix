ebe1a4ec3188d9f1dfeac08d90a02a853bac95d5
zeme <lorenzo.calegari@iohk.io>
2023-08-24 13:37:30 +0200

    Use pkgs.fetchFromGitHub instead of pkgs.fetchTarball

--- src/modules/haskell/ext/haskell-language-server-project-ghc962.nix
+++ src/modules/haskell/ext/haskell-language-server-project-ghc962.nix
@@ -20,9 +20,11 @@ pkgs.haskell-nix.cabalProject' {
     constraints: stylish-haskell==0.14.5.0, hlint==3.6.1
   '';
 
-  src = pkgs.fetchTarball {
-    url = https://github.com/haskell/haskell-language-server/archive/refs/tags/2.1.0.0.tar.gz;
-    sha256 = "sha256:1ivqj503al44nnilmpqd916ds5cl7hcxy4jm94ahi8y13v9p8r7y";
+  src = pkgs.fetchFromGitHub {
+    owner = "haskell";
+    repo = "haskell-language-server";
+    rev = "2.1.0.0";
+    sha256 = "sha256-/mR00x7BowgVSVUS3xk8lBXdTEgN30qjtYRQNUCReMc=";
   };
 
   compiler-nix-name = "ghc962";

5d1a002e217456efbaf7ea9459f78b8fe3bdfe3e
zeme <lorenzo.calegari@iohk.io>
2023-09-28 10:14:30 +0200

    wip

--- src/core/mkHaskellProject.nix
+++ src/core/mkHaskellProject.nix
@@ -139,7 +139,7 @@ let
           project.hydraJobs // 
           extra-packages // 
           utils.mapAttrValues (project: project.hydraJobs) project.variants //
-          { required = repoRoot.src.core.mkRequiredHydraJob {}; } //
+          { required = repoRoot.src.core.mkHydraRequiredJob {}; } //
           lib.optionalAttrs 
             (system == "x86_64-linux" && haskellProject.crossCompileMingwW64Supported)
             { mingwW64 = project.cross.mingwW64.hydraJobs; }; 

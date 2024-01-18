f182168310b81b311b400204249cce52dcedd764
zeme <lorenzo.calegari@iohk.io>
2024-01-18 10:01:57 +0100

    wip

--- scripts/bump-iogx-everywhere.sh
+++ scripts/bump-iogx-everywhere.sh
@@ -20,7 +20,9 @@ iogx_vbump_repo() {
   git checkout -b "iogx-bump-$vbump_tag" || git checkout "iogx-bump-$vbump_tag"
   git pull --rebase origin "$repo_branch"
   if [ "$custom_iogx_branch" != "" ]; then
-    sed -i '' "'s|github:input-output-hk/iogx|github:input-output-hk/iogx?ref=$custom_iogx_branch|'" flake.nix
+    if ! [ grep -q "github:input-output-hk/iogx?ref=$custom_iogx_branch" flake.nix ]; then
+      sed -i "s|github:input-output-hk/iogx|github:input-output-hk/iogx?ref=$custom_iogx_branch|" flake.nix
+    fi
   fi
   nix flake lock --update-input iogx
   git add .
@@ -37,44 +39,44 @@ iogx_vbump_repo() {
 
 
 iogx_vbump_plutus() { iogx_vbump_repo master plutus yes; }
-iogx_vbump_plutus_apps() { iogx_vbump_repo main plutus-apps yes; }
-iogx_vbump_marlowe_ts_sdk() { iogx_vbump_repo main marlowe-ts-sdk no; }
-iogx_vbump_marlowe_agda() { iogx_vbump_repo main marlowe-agda no; }
+iogx_vbump_plutus-apps() { iogx_vbump_repo main plutus-apps yes; }
+iogx_vbump_marlowe-ts-sdk() { iogx_vbump_repo main marlowe-ts-sdk no; }
+iogx_vbump_marlowe-agda() { iogx_vbump_repo main marlowe-agda no; }
 iogx_vbump_marconi() { iogx_vbump_repo main marconi no; }
-iogx_vbump_dapps_certification() { iogx_vbump_repo master dapps-certification no; }
-iogx_vbump_marconi_sidechain_node() { iogx_vbump_repo main marconi-sidechain-node no; }
-iogx_vbump_marlowe() { iogx_vbump_repo main marlowe no; }
-iogx_vbump_quickcheck_dynamic() { iogx_vbump_repo main quickcheck-dynamic no; }
-iogx_vbump_marlowe_token_plans() { iogx_vbump_repo main marlowe-token-plans no; }
-iogx_vbump_marlowe_runner() { iogx_vbump_repo main marlowe-runner no; }
-iogx_vbump_marlowe_plutus() { iogx_vbump_repo main marlowe-plutus no; }
-iogx_vbump_marlowe_payouts() { iogx_vbump_repo main marlowe-payouts no; }
-iogx_vbump_marlowe_cardano() { iogx_vbump_repo main marlowe-cardano no; }
-iogx_vbump_stablecoin_plutus() { iogx_vbump_repo main stablecoin-plutus no; }
-iogx_vbump_quickcheck_contractmodel() { iogx_vbump_repo master quickcheck-contractmodel no; }
+iogx_vbump_dapps-certification() { iogx_vbump_repo master dapps-certification no; }
+iogx_vbump_marconi-sidechain-node() { iogx_vbump_repo main marconi-sidechain-node no; }
+iogx_vbump_marlowe() { iogx-vbump-repo main marlowe no; }
+iogx_vbump_quickcheck-dynamic() { iogx_vbump_repo main quickcheck-dynamic no; }
+iogx_vbump_marlowe-token-plans() { iogx_vbump_repo main marlowe-token-plans no; }
+iogx_vbump_marlowe-runner() { iogx_vbump_repo main marlowe-runner no; }
+iogx_vbump_marlowe-plutus() { iogx_vbump_repo main marlowe-plutus no; }
+iogx_vbump_marlowe-payouts() { iogx_vbump_repo main marlowe-payouts no; }
+iogx_vbump_marlowe-cardano() { iogx_vbump_repo main marlowe-cardano no; }
+iogx_vbump_stablecoin-plutus() { iogx_vbump_repo main stablecoin-plutus no; }
+iogx_vbump_quickcheck-contractmodel() { iogx_vbump_repo master quickcheck-contractmodel no; }
 iogx_vbump_antaeus() { iogx_vbump_repo main antaeus no; }
-iogx_vbump_marlowe_playground() { iogx_vbump_repo main marlowe-playground no; }
+iogx_vbump_marlowe-playground() { iogx_vbump_repo main marlowe-playground no; }
 
 
 iogx_vbump_everywhere() {
-  iogx_vbump_plutus                    
-  iogx_vbump_plutus_apps               
-  iogx_vbump_marlowe_ts_sdk            
-  iogx_vbump_marlowe_agda              
-  iogx_vbump_marconi                   
-  iogx_vbump_dapps_certification       
-  iogx_vbump_marconi_sidechain_node    
-  iogx_vbump_marlowe                   
-  iogx_vbump_quickcheck_dynamic        
-  iogx_vbump_marlowe_token_plans       
-  iogx_vbump_marlowe_runner            
-  iogx_vbump_marlowe_plutus            
-  iogx_vbump_marlowe_payouts           
-  iogx_vbump_marlowe_cardano           
-  iogx_vbump_stablecoin_plutus         
-  iogx_vbump_quickcheck_contractmodel  
-  iogx_vbump_antaeus                   
-  iogx_vbump_marlowe_playground        
+  iogx_vbump_plutus
+  iogx_vbump_plutus-apps
+  iogx_vbump_marlowe-ts-sdk
+  iogx_vbump_marlowe-agda
+  iogx_vbump_marconi
+  iogx_vbump_dapps-certification
+  iogx_vbump_marconi-sidechain-node
+  iogx_vbump_marlowe
+  iogx_vbump_quickcheck-dynamic
+  iogx_vbump_marlowe-token-plans
+  iogx_vbump_marlowe-runner
+  iogx_vbump_marlowe-plutus
+  iogx_vbump_marlowe-payouts
+  iogx_vbump_marlowe-cardano
+  iogx_vbump_stablecoin-plutus
+  iogx_vbump_quickcheck-contractmodel
+  iogx_vbump_antaeus
+  iogx_vbump_marlowe-playground        
 }
 
 

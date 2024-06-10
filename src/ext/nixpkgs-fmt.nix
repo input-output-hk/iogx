5b4e113f1a59f374bcb2305b2b768b61c8298dfa
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2024-06-10 07:08:58 +0200

    Bump nixpkgs-fmt 1.1.0 -> 1.3.0 (#70)

--- src/ext/nixpkgs-fmt.nix
+++ src/ext/nixpkgs-fmt.nix
@@ -4,17 +4,16 @@ pkgs.rustPlatform.buildRustPackage rec {
 
   pname = "nixpkgs-fmt";
 
-  # nixpkgs-fmt 1.2.0 breaks indentation of code examples in comments
-  version = "1.1.0";
+  version = "1.3.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "nix-community";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1fb2mm1y2qb3imc48g2ad3rdbjlj326cggrc4hvdc0fb41vxinpp";
+    sha256 = "sha256-6Ut4/ix915EoaPCewoG3KhKBA+OaggpDqnx2nvKxEpQ=";
   };
 
-  cargoSha256 = "1lsw6dwkjdwdqcx7gjsg2ndi4r79m8qyxgx7yz3al0iscwm7i645";
+  cargoSha256 = "sha256-yIwCBm46sgrpTt45uCyyS7M6V0ReGUXVu7tyrjdNqeQ=";
 
   meta = with lib; {
     description = "Nix code formatter for nixpkgs";

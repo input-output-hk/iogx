9c1f6c1cac78b7a1e88ef9bc7833f59ae79b4225
zeme <lorenzo.calegari@iohk.io>
2024-07-04 14:39:08 +0200

    wip

--- src/ext/haskell-language-server-project.nix
+++ src/ext/haskell-language-server-project.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ repoRoot, lib, pkgs, ... }:
 
 # Builds haskell-language-server using the given ghc version.
 
@@ -33,11 +33,18 @@ let
         cabalProjectLocal = "constraints: stylish-haskell ^>= 0.14, hlint ^>= 3.8";
         configureArgs = "--disable-benchmarks";
       }
-    # TODO add `else if lib.hasInfix "ghc910" ghc` when HLS v2.9.0.0 supports hlint/stylish-haskell for ghc910
+    else if lib.hasInfix "ghc910" ghc then
+      {
+        rev = "2.9.0.0";
+        sha256 = "sha256-THXSz+iwB1yQQsr/PY151+2GvtoJnTIB2pIQ4OzfjD4=";
+        configureArgs = "--disable-benchmarks";
+        # TODO remove this once HLS fully supports ghc910 with hlint and stylish-haskell
+        hls-fallback = repoRoot.src.ext.haskell-language-server-project "ghc98";
+      }
     else
       lib.trace
         ''
-          Unsupported GHC version ${ghc}, defaulting to ghc98 for haskell-language-server v2.8.0.0
+          Unsupported GHC version ${ghc}, defaulting to ghc98 and haskell-language-server v2.8.0.0
         ''
         {
           ghc = "ghc98";
@@ -47,41 +54,53 @@ let
           configureArgs = "--disable-benchmarks";
         };
 
+
+  project = pkgs.haskell-nix.cabalProject' {
+    # See https://github.com/haskell/haskell-language-server/issues/411.
+    # We want to use stylish-haskell, hlint, and implicit-hie as standalone tools
+    # *and* through HLS. But we need to have consistent versions in both cases,
+    # otherwise e.g. you could format the code in HLS and then have the CI
+    # complain that it's wrong
+    #
+    # The solution we use here is to:
+    # a) Where we care (mostly just formatters), constrain the versions of
+    #    tools which HLS uses explicitly
+    # b) Pull out the tools themselves from the HLS project so we can use
+    #    them elsewhere
+    cabalProjectLocal = config.cabalProjectLocal or "";
+
+    configureArgs = config.configureArgs or "";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "haskell";
+      repo = "haskell-language-server";
+      inherit (config) rev sha256;
+    };
+
+    compiler-nix-name = ghc;
+
+    sha256map = {
+      "https://github.com/pepeiborra/ekg-json"."7a0af7a8fd38045fd15fb13445bdcc7085325460" = "sha256-fVwKxGgM0S4Kv/4egVAAiAjV7QB5PBqMVMCfsv7otIQ="; # editorconfig-checker-disable-line
+    };
+
+    modules = [{
+      # See https://github.com/haskell/haskell-language-server/pull/1382#issuecomment-780472005
+      packages.ghcide.flags.ghc-patched-unboxed-bytecode = true;
+
+      dontStrip = false;
+    }];
+  };
+
+
+  project' =
+    let
+      hls-fallback = config.hls-fallback or project;
+    in
+    lib.recursiveUpdate project {
+      hsPkgs.hlint = hls-fallback.hsPkgs.hlint;
+      hsPkgs.stylish-haskell = hls-fallback.hsPkgs.stylish-haskell;
+    };
+
 in
 
-pkgs.haskell-nix.cabalProject' {
-
-  # See https://github.com/haskell/haskell-language-server/issues/411.
-  # We want to use stylish-haskell, hlint, and implicit-hie as standalone tools
-  # *and* through HLS. But we need to have consistent versions in both cases,
-  # otherwise e.g. you could format the code in HLS and then have the CI
-  # complain that it's wrong
-  #
-  # The solution we use here is to:
-  # a) Where we care (mostly just formatters), constrain the versions of
-  #    tools which HLS uses explicitly
-  # b) Pull out the tools themselves from the HLS project so we can use
-  #    them elsewhere
-  cabalProjectLocal = config.cabalProjectLocal or "";
-
-  configureArgs = config.configureArgs or "";
-
-  src = pkgs.fetchFromGitHub {
-    owner = "haskell";
-    repo = "haskell-language-server";
-    inherit (config) rev sha256;
-  };
-
-  compiler-nix-name = config.ghc or ghc;
-
-  sha256map = {
-    "https://github.com/pepeiborra/ekg-json"."7a0af7a8fd38045fd15fb13445bdcc7085325460" = "sha256-fVwKxGgM0S4Kv/4egVAAiAjV7QB5PBqMVMCfsv7otIQ="; # editorconfig-checker-disable-line
-  };
-
-  modules = [{
-    # See https://github.com/haskell/haskell-language-server/pull/1382#issuecomment-780472005
-    packages.ghcide.flags.ghc-patched-unboxed-bytecode = true;
-
-    dontStrip = false;
-  }];
-}
+project' 

e6148add2debfa212e3288d789e76969b49d2fde
zeme <lorenzo.calegari@iohk.io>
2025-07-09 14:59:20 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system: 
+  outputs = inputs: inputs.flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (system: 
     let
       pkgs = inputs.nixpkgs.legacyPackages.${system};
     in 
